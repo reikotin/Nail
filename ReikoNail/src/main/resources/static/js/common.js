e4ded9b613be71a8e153a0f71dff4c6ee469a5e1
sfsf700 <hirokazu.2rnk@gmail.com>
2024-03-26 22:00:29 +0900

    日付js共通化

--- ReikoNail/src/main/resources/static/js/common.js
+++ ReikoNail/src/main/resources/static/js/common.js
@@ -1,6 +1,14 @@
 /**
  * 
  */
+
+$(document).ready(function () {
+	updateClock();
+	setInterval(updateClock, 1000);
+	
+	console.log('共通JS');
+	
+});
 function updateClock() {
 	var now = new Date();
 	var formattedTime = now.toLocaleString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false });
